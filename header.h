349fcb1cafe0ce80a371abab246286f4811a627e
Nick van den Dungen <nicktvdd@hotmail.com>
2023-05-02 14:21:53 +0300

    norm header

--- header.h
+++ header.h
@@ -1,9 +1,21 @@
-#ifndef HEADER_H
-#define HEADER_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   header.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nvan-den <nvan-den@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/02 14:20:28 by nvan-den          #+#    #+#             */
+/*   Updated: 2023/05/02 14:20:30 by nvan-den         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include <unistd.h>
-#include <signal.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include "./libft/libft.h"
+#ifndef HEADER_H
+# define HEADER_H
+
+# include <unistd.h>
+# include <signal.h>
+# include <stdlib.h>
+# include <sys/types.h>
+# include "./libft/libft.h"
 #endif
