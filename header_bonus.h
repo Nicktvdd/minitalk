465e2f7efd75cba2a404c328494a87ef1d1e9422
Nick van den Dungen <nicktvdd@hotmail.com>
2023-05-02 15:39:35 +0300

    bonus header protection

--- header_bonus.h
+++ header_bonus.h
@@ -6,12 +6,12 @@
 /*   By: nvan-den <nvan-den@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 14:45:10 by nvan-den          #+#    #+#             */
-/*   Updated: 2023/05/02 14:45:48 by nvan-den         ###   ########.fr       */
+/*   Updated: 2023/05/02 15:39:23 by nvan-den         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef HEADER_H
-# define HEADER_H
+#ifndef HEADER_BONUS_H
+# define HEADER_BONUS_H
 
 # include <unistd.h>
 # include <signal.h>
