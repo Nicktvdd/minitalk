16e0a8866546d75ca4eb35e822cd8b01dd6b8620
Nick van den Dungen <nicktvdd@hotmail.com>
2023-05-02 14:13:45 +0300

    client split into another function

--- client.c
+++ client.c
@@ -6,11 +6,32 @@
 /*   By: nvan-den <nvan-den@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 19:18:28 by nick              #+#    #+#             */
-/*   Updated: 2023/04/28 16:11:17 by nvan-den         ###   ########.fr       */
+/*   Updated: 2023/05/02 14:11:12 by nvan-den         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./header.h"
+
+int	signal_sender(char *string, int pid, int i)
+{
+	int		bit_pos;
+	char	character;
+	
+	bit_pos = 0;
+	character = string[i];
+	while (bit_pos < 8)
+	{
+		if (character & (1 << bit_pos))
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		usleep(100);
+		bit_pos++;
+	}
+	bit_pos = 0;
+	i++;
+	return (i);
+}
 
 void sig_handler(int sig) 
 {
@@ -23,16 +44,8 @@ int	main(int argc, char **argv)
 	int		i;
 	int		pid;
 	char	*string;
-	char 	character;
-	int		bit_pos;
-
+	
 	i = 0;
-	bit_pos = 0;
-	if (argc != 3) 
-	{
-        ft_printf("Usage: %s [server_pid] [string_to_send]\n", argv[0]);
-        return(1);
-	}
 	pid = ft_atoi(argv[1]);
 	string = argv[2];
 	string[ft_strlen(string)] = '\0';
@@ -42,33 +55,8 @@ int	main(int argc, char **argv)
 	sigact.sa_handler = sig_handler;
 	sigaction(SIGUSR1, &sigact, NULL);
 	sigaction(SIGUSR2, &sigact, NULL);
-	
-	while (string[i])
-	{
-		character = string[i];
-		while (bit_pos < 8)
-		{
-			if (character & (1 << bit_pos))
-				kill(pid, SIGUSR1);
-			else
-				kill(pid, SIGUSR2);
-			usleep(100);
-			bit_pos++;
-		}
-		bit_pos = 0;
-		i++;
-	}
- 	character = string[i];
-	while (bit_pos < 8)
-	{
-		if (character & (1 << bit_pos))
-			kill(pid, SIGUSR1);
-		else
-			kill(pid, SIGUSR2);
-		usleep(100);
-		bit_pos++;
-	}
-	bit_pos = 0;
-	i++; 
+	while (string[i] && (argc))
+		i = signal_sender(string, pid, i);
+	signal_sender(string, pid, i);
 	return(0);
 }
